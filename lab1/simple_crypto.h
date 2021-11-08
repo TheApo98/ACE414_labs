df3f0e858bc0eb52dea8ef5a7e96d25c36eeab0f
TheApo98 <73966912+TheApo98@users.noreply.github.com>
2021-11-09 00:01:48 +0200

    Declared 'Vigenère’s cipher' functions

--- lab1/simple_crypto.h
+++ lab1/simple_crypto.h
@@ -3,3 +3,5 @@ char * one_time_pad_ENCR(char * msg);
 char * one_time_pad_DECR(char * encrMsg);
 char * ceasars_cipher_ENCR(char * msg, int key);
 char * ceasars_cipher_DECR(char * encMsg, int key);
+char * vigeneres_cipher_ENCR(char * msg, char * key);
+char * vigeneres_cipher_DECR(char * encMsg, char * key);
