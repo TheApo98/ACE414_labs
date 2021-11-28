bdf2a40033d0cca80531fd38017c09816e9c8688
TheApo98 <73966912+TheApo98@users.noreply.github.com>
2021-11-28 20:49:27 +0200

    Added some test cases

--- lab3/src_corpus/test_aclog.c
+++ lab3/src_corpus/test_aclog.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <stdlib.h>       
+#include <unistd.h>
+
 
 int main() 
 {
@@ -25,7 +30,65 @@ int main()
 		}
 
 	}
+	
+	struct stat stats;
+    // int filedes = fileno(file);
+    // if(fstat(filedes, &stats) == -1){
+    //     fprintf(stderr, "Stat() failed: \n%s!\n", strerror(errno));
+    //     exit(EXIT_FAILURE);
+    // }
 
+	printf("Cur UID: %d\n", getuid());
+	for (i = 0; i < 10; i++) {
+		// printf("File: %s, UID: %d\n", filenames[i], stats.st_uid);
+		chmod(filenames[i],  S_IRUSR|S_IROTH|S_IRGRP );	// -r--r--r-
+		file = fopen(filenames[i], "w+");
+		if (file == NULL) 
+			printf("fopen error\n");
+		else {
+			bytes = fwrite(filenames[i], strlen(filenames[i]), 1, file);
+			fclose(file);
+		}
+
+	}
+
+	// printf("Cur UID: %d\n", getuid());
+	for (i = 0; i < 10; i++) {
+		// printf("File: %s, UID: %d\n", filenames[i], stats.st_uid);
+		chmod(filenames[i],  S_IWUSR|S_IWOTH|S_IWGRP | S_IRUSR|S_IROTH|S_IRGRP );	// wr-wr-wr-
+		file = fopen(filenames[i], "r");
+		if (file == NULL) 
+			printf("fopen error\n");
+		else {
+			bytes = fwrite(filenames[i], strlen(filenames[i]), 1, file);
+			fclose(file);
+		}
+
+	}
+
+	// printf("Cur UID: %d\n", getuid());
+	for (i = 0; i < 10; i++) {
+		// printf("File: %s, UID: %d\n", filenames[i], stats.st_uid);
+		chmod(filenames[i],  S_IWUSR|S_IWOTH|S_IWGRP | S_IRUSR|S_IROTH|S_IRGRP );	// wr-wr-wr-
+		// chmod(filenames[i],  S_IRUSR|S_IROTH|S_IRGRP );	// -r--r--r-
+		file = fopen(filenames[i], "a+");
+		if (file == NULL) 
+			printf("fopen error\n");
+		else {
+			bytes = fwrite(filenames[i], strlen(filenames[i]), 1, file);
+			fclose(file);
+		}
+
+	}
+
+	// char filename[] = "test3.txt";
+	// file = fopen(filename, "r+");
+	// if (file == NULL) 
+	// 	printf("fopen error\n");
+	// else {
+	// 	bytes = fwrite(filename, strlen(filename), 1, file);
+	// 	fclose(file);
+	// }
 
 	/* add your code here */
 	/* ... */
