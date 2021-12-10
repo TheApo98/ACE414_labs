efd2562655cb27f67a7eccea2ecb4bfe4ab7ca51
TheApo98 <73966912+TheApo98@users.noreply.github.com>
2021-12-11 01:26:08 +0200

    Necessary files from lab3

---
+++ lab5/src_corpus/acmonitor.c
@@ -0,0 +1,434 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+// Added libraries
+#include <errno.h>
+#include <openssl/md5.h>
+
+struct entry {
+
+	int uid; /* user id (positive integer) */
+	int access_type; /* access type values [0-2] */
+	int action_denied; /* is action denied values [0-1] */
+
+	time_t date; /* file access date */
+	time_t time; /* file access time */
+
+	char *file; /* filename (string) */
+	char *fingerprint; /* file fingerprint */
+
+	/* add here other fields if necessary */
+	/* ... */
+	/* ... */
+
+};
+
+// Useful functions from Assignment 2
+int readFromFile(FILE *fp, unsigned char * data, int * data_len){
+    /* File commands */ 
+    /* (necessary for reading special characters like EOF, etc) */
+    fseek(fp, 0, SEEK_END);     // go to file end
+    *data_len = ftell(fp);           // calculate the file size
+    rewind(fp);                 // go to file start and...
+    if(fread(data, *data_len, sizeof(unsigned char), fp) == 0){
+        fclose(fp);
+        return 1;
+    }
+    fclose(fp);
+    return 0;
+}
+
+void print_hex(unsigned char *data, size_t len)
+{
+	size_t i;
+
+	if (!data)
+		printf("NULL data\n");
+	else {
+		for (i = 0; i < len; i++) {
+			if (!(i % 16) && (i != 0))
+				printf("\n");
+			printf("%02X ", data[i]);
+		}
+		printf("\n");
+	}
+}
+
+void print_string(unsigned char *data, size_t len)
+{
+	size_t i;
+
+	if (!data)
+		printf("NULL data\n");
+	else {
+		for (i = 0; i < len; i++)
+			printf("%c", data[i]);
+		printf("\n");
+	}
+}
+
+
+void
+usage(void)
+{
+	printf(
+	       "\n"
+	       "usage:\n"
+	       "\t./monitor \n"
+		   "Options:\n"
+		   "-m, Prints malicious users\n"
+		   "-i <filename>, Prints table of users that modified "
+		   "the file <filename> and the number of modifications\n"
+		   "-h, Help message\n\n"
+		   );
+
+	exit(1);
+}
+
+
+void 
+list_unauthorized_accesses(FILE *log)
+{
+
+	/* add your code here */
+	/* ... */
+	/* ... */
+	/* ... */
+	/* ... */
+
+	char* data = (unsigned char*)malloc(sizeof(char)*256);
+	size_t data_len = 0;
+	// if(readFromFile(log, data, (int*)&data_len) == 1){
+    //     fprintf(stderr, "Error reading from file, errno: \n%s!\n", strerror(errno));
+    //     exit(EXIT_FAILURE);
+    // }
+	
+	struct entry *logs = (struct entry *)malloc(sizeof(struct entry)*1000);
+	// if(getline(&data, &data_len, log) == 0){
+	// 	fprintf(stderr, "Error!!! %s.\n", strerror(errno));
+    //     exit(EXIT_FAILURE);
+	// }
+    int i = 0;
+    int res = 0;
+
+    int * uids = (int*)malloc(sizeof(int) * 100);
+    int uids_len = 0;
+    int uid_exists = 0;
+
+	// Discard first line
+    if((res = getline(&data, &data_len, log)) == -1) return; 
+
+    // Read logs from file line-by-line and store the in struct array
+    while ((res = getline(&data, &data_len, log)) != -1) 
+    {
+        logs[i].file = (char*)malloc(sizeof(char)*100);        
+        logs[i].fingerprint = (char*)malloc(sizeof(char)*MD5_DIGEST_LENGTH*2);        
+        char* date;// = (unsigned char*)malloc(sizeof(char)*15);
+        char* time;// = (unsigned char*)malloc(sizeof(char)*15);
+        logs[i].uid = atoi(strtok(data, "|"));
+        strcpy(logs[i].file, strtok(NULL, "|"));  
+        date = strtok(NULL, "|");
+        time = strtok(NULL, "|");
+        logs[i].access_type = atoi(strtok(NULL, "|"));
+        logs[i].action_denied = atoi(strtok(NULL, "|"));
+        strcpy(logs[i].fingerprint, strtok(NULL, "|"));
+
+            // Find unique UIDs
+        // If the list is empty...
+        if(uids_len == 0){
+            // ... add a uid
+            uids[uids_len] = logs[i].uid;
+            /// And increase the size
+            uids_len++;
+        }
+        // If it's not empty....
+        else {
+            // ....compare the uid[i] to the uids in the list
+            int j;
+            for(j=0; j<uids_len; j++){
+                if(uids[j] == logs[i].uid){
+                    uid_exists = 1;
+                    break;
+                }
+            }
+            // If the uid is unique (not present in the list)...
+            if(uid_exists != 1){
+                // ...add the uid
+                uids[uids_len] = logs[i].uid;
+                // Increase list size
+                uids_len++;
+            }
+            // Restore value for next iteration
+            uid_exists = 0;
+        }
+        
+        i++;
+    }
+
+    // Get the size of the struct array
+    int logs_len = i;
+
+    // Print all uids for debugging
+    // printf("UIDs length: %d\n", uids_len);
+    // for (i = 0; i < uids_len; i++)
+    // {
+    //     printf("UID %d: %d\n", i, *(uids+i));
+    // }
+        
+    // Create an array for unique files (lets call it 'list')
+    char ** items = (char**)malloc(sizeof(char*) * logs_len);
+    
+    // The size of that array
+    int items_len = 0;
+
+    // If a files has multiple accesses from one user --> 1
+    int exists = 0;
+
+	// Just to print a message if no malicious uid exists
+	int malicious = 0;
+
+    for(int k=0; k<uids_len; k++){
+
+        // Iterate for every file in the logs
+        for (i = 0; i < logs_len; i++) {
+
+            // We care for one user at a time and only if he/she has no rights
+            if(logs[i].uid == uids[k] && logs[i].action_denied == 1){
+                // If the list is empty...
+                if(items_len == 0){
+                    // ... add a file
+                    items[items_len] = logs[i].file;
+                    /// And increase the size
+                    items_len++;
+                }
+                // If it's not empty....
+                else {
+                    // ....compare the file[i] to the files in the list
+                    for(int j=0; j<items_len; j++){
+                        exists |= (strcmp(items[j], logs[i].file) == 0);
+                    }
+                    // If the file is unique (not present in the list)...
+                    if(exists != 1){
+                        // ...add the file
+                        items[items_len] = logs[i].file;
+                        // Increase list size
+                        items_len++;
+                    }
+                    // Restore value for next iteration
+                    exists = 0;
+                }
+            }
+        }
+		// User accessed more than 7 files without auth
+        if(items_len >= 7){
+            printf("Malicious UID: %d\t", uids[k]);
+            // Print all files for each uid (for debugging)
+            printf("Accessed Files: %d\n", items_len);
+            // for (i = 0; i < items_len; i++)
+            // {
+            //     printf("File%d: %s\n", i, *(items+i));
+            // }
+			malicious |= 1;
+        }
+
+        // Restore the list size
+        items_len = 0;
+    }
+	if(!malicious)
+		printf("No malicious UID found\n");
+
+    free(data);
+    free(logs);
+    free(uids);
+    free(items);
+	return;
+
+}
+
+
+void
+list_file_modifications(FILE *log, char *file_to_scan)
+{
+    char* data = (unsigned char*)malloc(sizeof(char)*256);
+	size_t data_len = 0;
+	
+	struct entry *logs = (struct entry *)malloc(sizeof(struct entry)*1000);
+
+    int i = 0;
+    int res = 0;
+
+    int * uids = (int*)malloc(sizeof(int) * 100);
+    int uids_len = 0;
+    int uid_exists = 0;
+
+	// Discard first line
+    if((res = getline(&data, &data_len, log)) == -1) return; 
+
+    // Read logs from file line-by-line and store the in struct array
+    while ((res = getline(&data, &data_len, log)) != -1) 
+    {
+        logs[i].file = (char*)malloc(sizeof(char)*100);        
+        logs[i].fingerprint = (char*)malloc(sizeof(char)*MD5_DIGEST_LENGTH*2);        
+        char* date;// = (unsigned char*)malloc(sizeof(char)*15);
+        char* time;// = (unsigned char*)malloc(sizeof(char)*15);
+        logs[i].uid = atoi(strtok(data, "|"));
+        strcpy(logs[i].file, strtok(NULL, "|"));  
+        date = strtok(NULL, "|");
+        time = strtok(NULL, "|");
+        logs[i].access_type = atoi(strtok(NULL, "|"));
+        logs[i].action_denied = atoi(strtok(NULL, "|"));
+        strcpy(logs[i].fingerprint, strtok(NULL, "|"));
+
+            // Find unique UIDs
+        if(strcmp(logs[i].file, file_to_scan) == 0){
+        
+            // If the list is empty...
+            if(uids_len == 0){
+                // ... add a uid
+                uids[uids_len] = logs[i].uid;
+                /// And increase the size
+                uids_len++;
+            }
+            // If it's not empty....
+            else {
+                // ....compare the uid[i] to the uids in the list
+                int j;
+                for(j=0; j<uids_len; j++){
+                    if(uids[j] == logs[i].uid){
+                        uid_exists = 1;
+                        break;
+                    }
+                }
+                // If the uid is unique (not present in the list)...
+                if(uid_exists != 1){
+                    // ...add the uid
+                    uids[uids_len] = logs[i].uid;
+                    // Increase list size
+                    uids_len++;
+                }
+                // Restore value for next iteration
+                uid_exists = 0;
+            }
+        }
+        i++;
+    }
+
+    // Get the size of the struct array
+    int logs_len = i;
+
+    // // Print every log (for debugging)
+    // for(int j=0; j<i; j++){
+    //     printf("\tLog entry: %d\n", j+1);
+    //     printf("UID: %d\n", logs[j].uid);
+    //     printf("File name: %s\n", logs[j].file);
+    //     // printf("Date: %s\n", date);
+    //     // printf("Timestamp: %s\n", time);
+    //     printf("Access Type: %d\n", logs[j].access_type);
+    //     printf("Action denied flag: %d\n", logs[j].action_denied);
+    //     printf("Fingerprint(MD5): %s\n", logs[j].fingerprint);
+    //     // print_hex((unsigned char*)logs[j].fingerprint, MD5_DIGEST_LENGTH);
+    //     // print_string((unsigned char*)logs[j].fingerprint, MD5_DIGEST_LENGTH);
+    //     printf("\n\n"); 
+    // }
+
+
+    // Print all uids for debugging
+    // printf("UIDs length: %d\n", uids_len);
+    // for (i = 0; i < uids_len; i++)
+    // {
+    //     printf("UID %d: %d\n", i, *(uids+i));
+    // }
+    
+
+    char * prev_hash;
+
+    // The size of that array
+    int hashes_len = 0;
+
+    // If a files has multiple accesses from one user --> 1
+    int exists = 0;
+
+    for(int k=0; k<uids_len; k++){
+		for (i = 0; i < logs_len; i++) {
+			if(logs[i].uid == uids[k] && strcmp(logs[i].file , file_to_scan) == 0){
+				prev_hash = logs[i].fingerprint;
+				break;
+			}
+		}
+        // Iterate for every file in the logs
+        for (i = 0; i < logs_len; i++) {
+
+            // We care for one user at a time and only for a specific file
+            if(logs[i].uid == uids[k] && strcmp(logs[i].file , file_to_scan) == 0){
+				// printf("%s Vs %s\n", prev_hash, logs[i].fingerprint);
+				if(strcmp(prev_hash, logs[i].fingerprint) != 0){
+					hashes_len++;
+				}
+				prev_hash = logs[i].fingerprint;
+            }
+        }
+        printf("File: %s\n", file_to_scan);
+        
+        printf("UID: %d", uids[k]);
+        printf("\tTimes modified: %d\n", hashes_len);
+        
+
+        // Restore the list size
+        hashes_len = 0;
+
+    }
+	free(data);
+    free(logs);
+    free(uids);
+	return;
+
+}
+
+
+int 
+main(int argc, char *argv[])
+{
+
+	int ch;
+	FILE *log;
+
+	if (argc < 2)
+		usage();
+
+	log = fopen("./file_logging.log", "r");
+	if (log == NULL) {
+		printf("Error opening log file \"%s\"\n", "./log");
+		return 1;
+	}
+
+	while ((ch = getopt(argc, argv, "hi:m")) != -1) {
+		switch (ch) {		
+		case 'i':
+			list_file_modifications(log, optarg);
+			break;
+		case 'm':
+			list_unauthorized_accesses(log);
+			break;
+		default:
+			usage();
+		}
+
+	}
+
+
+	/* add your code here */
+	/* ... */
+	/* ... */
+	/* ... */
+	/* ... */
+
+
+	fclose(log);
+	argc -= optind;
+	argv += optind;	
+	
+	return 0;
+}
---
+++ lab5/src_corpus/logger.c
@@ -0,0 +1,340 @@
+#define _GNU_SOURCE
+
+#include <time.h>
+#include <stdio.h>
+#include <dlfcn.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <openssl/md5.h>
+
+// Added libraries
+#include <errno.h>
+#include <string.h>
+
+#define FILE_CREATE	0
+#define FILE_OPEN 	1
+#define FILE_WRITE 	1
+
+const char log_file[] = "file_logging.log";
+
+struct entry {
+
+	int uid; /* user id (positive integer) */
+	char *file; /* filename (string) */
+
+	// time_t date; /* file access date */
+	time_t time; /* file access time */
+
+	int access_type; /* access type values [0-2] */
+	int action_denied; /* is action denied values [0-1] */
+
+	char *fingerprint; /* file fingerprint */
+
+};
+
+// Declare functions here
+int readFromFile(char * filename, unsigned char * data, int * data_len);
+int writeToFile(char * filename, unsigned char * data, int data_len);
+void formatDateTime(struct tm* tm_ptr, char * date, char * time);
+int writeLogsToFile(struct entry logs);
+char * getFilename(FILE *fp);
+struct tm * getDateTime(time_t t);
+void string_to_hex(unsigned char *data, char *out_data, size_t len);
+
+
+
+FILE *
+fopen(const char *path, const char *mode) 
+{
+
+	FILE *original_fopen_ret;
+	FILE *(*original_fopen)(const char*, const char*);
+
+	// Code executed before fopen_original
+	int file_exists = access(path, F_OK) + 1;
+
+	/* call the original fopen function */
+	original_fopen = dlsym(RTLD_NEXT, "fopen");
+	original_fopen_ret = (*original_fopen)(path, mode);
+
+	// If fopen fails, just return?
+	if(original_fopen_ret == NULL){
+		// File doesn't exist (Probably mode="r+")
+		// printf("Error!!! %s, %d\n", strerror(errno), errno);
+	    struct entry logs;
+		logs.file =  (char *)path;
+		logs.time = time(NULL);
+		logs.uid = getuid();
+		logs.access_type = file_exists;
+		// Permission denied
+		if(errno == 13)
+			logs.action_denied = 1;
+		else
+			logs.action_denied = 0;
+		logs.fingerprint = "00000000000000000000000000000000";
+		if(writeLogsToFile(logs) == -1){
+			fprintf(stderr, "Error!!! %s.\n", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+		else{
+			// printf("[fopen] Writing logs successful\n");
+		}
+		return original_fopen_ret;
+	}
+
+	
+	// Create entry struct
+    struct entry logs;
+	// logs.file = (char *)path;
+	logs.file = getFilename(original_fopen_ret);
+    logs.time = time(NULL);
+	logs.uid = getuid();
+	logs.access_type = file_exists;
+	// If you got to this point, you have access
+	logs.action_denied = 0;
+    
+
+	// The MD5 hash from the file
+    unsigned char* md5_hash = (unsigned char*)malloc(MD5_DIGEST_LENGTH);
+    unsigned char* data = (unsigned char*)malloc(sizeof(char)*256);
+	size_t data_len = 0;
+	if(readFromFile(logs.file, data, (int*)&data_len) == 1){
+        fprintf(stderr, "Error reading from file, errno: \n%s!\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    // printf("After readFromFile\n");
+    // print_string(data, data_len);
+
+	logs.fingerprint = (char*)malloc(sizeof(char)*(MD5_DIGEST_LENGTH*2));
+	// If there are data in the file, generate MD5 hash.....
+	if(data_len > 0){
+		MD5(data, data_len, md5_hash);
+		// logs.fingerprint = (char *)md5_hash;
+		string_to_hex(md5_hash, logs.fingerprint, MD5_DIGEST_LENGTH);
+		// printf("After MD5\n");
+	}
+	//... if not, hash = '0'
+	else 
+		memcpy(logs.fingerprint, "00000000000000000000000000000000", MD5_DIGEST_LENGTH*2);
+
+    if(writeLogsToFile(logs) == -1){
+		fprintf(stderr, "Error!!! %s.\n", strerror(errno));
+        exit(EXIT_FAILURE);
+	}
+	else{
+		// printf("[fopen] Writing logs successful\n");
+	}
+	free(data);
+	free(md5_hash);
+	free(logs.file);
+	free(logs.fingerprint);
+
+	return original_fopen_ret;
+}
+
+
+size_t 
+fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream) 
+{
+	size_t original_fwrite_ret;
+	size_t (*original_fwrite)(const void*, size_t, size_t, FILE*);
+
+	/* call the original fwrite function */
+	original_fwrite = dlsym(RTLD_NEXT, "fwrite");
+	original_fwrite_ret = (*original_fwrite)(ptr, size, nmemb, stream);
+	
+	// Flush the output to file so we can read the new data
+	// We can't wait for fclose, we need them now!!!
+	fflush(stream);
+
+	// If fopen fails, just return?
+	// if(original_fwrite_ret == (int)nmemb)
+	// 	return original_fwrite_ret;
+	
+	// Create entry struct
+    struct entry logs;
+
+	logs.file = getFilename(stream);
+    logs.time = time(NULL);
+
+    // printf("After Time\n");
+
+	logs.uid = getuid();
+	logs.access_type = 2;
+
+	// Compare current uid and file uid
+	if(original_fwrite_ret < (int)nmemb)
+		logs.action_denied = 1;
+    else
+        logs.action_denied = 0;
+    // printf("After action_denied_flag\n");
+    
+	// The MD5 hash from the file
+    unsigned char* md5_hash = (unsigned char*)malloc(MD5_DIGEST_LENGTH);
+    unsigned char* data = (unsigned char*)malloc(sizeof(char)*256);
+	size_t data_len = 0;
+	if(readFromFile(logs.file, data, (int*)&data_len) == 1){
+        fprintf(stderr, "Error reading from file, errno: \n%s!\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    // printf("After readFromFile\n");
+    // print_string(data, data_len);
+
+    MD5(data, data_len, md5_hash);
+	logs.fingerprint = (char*)malloc(sizeof(char)*(MD5_DIGEST_LENGTH*2));
+	string_to_hex(md5_hash, logs.fingerprint, MD5_DIGEST_LENGTH);
+    // logs.fingerprint = (char *)md5_hash;
+    // printf("After MD5\n");
+
+    if(writeLogsToFile(logs) == -1){
+		fprintf(stderr, "Error!!! %s.\n", strerror(errno));
+        exit(EXIT_FAILURE);
+	}
+	else{
+		// printf("[fwrite] Writing logs successful\n");
+	}
+	free(data);
+	free(md5_hash);
+	free(logs.file);
+	free(logs.fingerprint);
+
+	return original_fwrite_ret;
+}
+
+
+/* My custom functions */
+
+char * getFilename(FILE *fp){
+    int filedes = fileno(fp);
+    int MAXSIZE = 0xFFF;
+    char * proclnk = (char *)malloc(sizeof(char)*MAXSIZE);
+    char * filename = (char *)malloc(sizeof(char)*MAXSIZE);
+    sprintf(proclnk, "/proc/self/fd/%d", filedes);
+    int r = readlink(proclnk, filename, MAXSIZE);
+    if (r < 0)
+    {
+        printf("Failed to readlink\n");
+        free(proclnk);
+        free(filename);
+        exit(1);
+    }
+    filename[r] = '\0';
+    free(proclnk);
+    // printf("File Name: %s\n", filename);
+	
+    char * relative_path = (char *)malloc(sizeof(char)*MAXSIZE);
+    char * token;
+	token = strtok(filename, "/");
+	while( token != NULL ) {
+		strcpy(relative_path , token);
+		token = strtok(NULL, "/");
+	}
+    free(filename);
+    return relative_path;
+    // return filename;
+}
+
+// Date and time
+struct tm * getDateTime(time_t t){
+	struct tm* tm_ptr;
+	// Convert it to local time and fill the struct
+    tm_ptr = localtime(&t);
+	// Convert using struct to human readable string
+    // char * dateTime = asctime(tm_ptr);
+    return tm_ptr;
+}
+
+void formatDateTime(struct tm* tm_ptr, char * date, char * time){
+    sprintf(date, "%02d-%02d-%d", tm_ptr->tm_mday, tm_ptr->tm_mon, tm_ptr->tm_year+1900);
+    sprintf(time, "%02d:%02d:%02d", tm_ptr->tm_hour, tm_ptr->tm_min, tm_ptr->tm_sec);
+}
+
+int writeLogsToFile(struct entry logs){
+	FILE *original_fopen_ret;
+	FILE *(*original_fopen)(const char*, const char*);
+
+	int file_exists = access(log_file, F_OK) + 1;
+	
+	/* call the original fopen function */
+	original_fopen = dlsym(RTLD_NEXT, "fopen");
+	original_fopen_ret = (*original_fopen)(log_file, "a");
+
+    if(original_fopen_ret == NULL){
+        return 1;
+    }
+    
+	
+	int wr_err = 0;
+	if(!file_exists){
+		wr_err = fprintf(original_fopen_ret, "UID | Filename | Date | Time | Access_Type | Action_Denied | Fingerprint |\n");
+	}
+
+    char * date = malloc(sizeof(char)*15);
+    char * time = malloc(sizeof(char)*15);
+    formatDateTime(getDateTime(logs.time), date, time);
+
+	wr_err = fprintf(original_fopen_ret, "%d|", logs.uid);
+	wr_err = fprintf(original_fopen_ret, "%s|", logs.file);
+	wr_err = fprintf(original_fopen_ret, "%s|", date);
+	wr_err = fprintf(original_fopen_ret, "%s|", time);
+	wr_err = fprintf(original_fopen_ret, "%d|", logs.access_type);
+	wr_err = fprintf(original_fopen_ret, "%d|", logs.action_denied);
+	wr_err = fprintf(original_fopen_ret, "%s|\n", logs.fingerprint);
+	free(date);
+	free(time);
+	fclose(original_fopen_ret);
+	// Give permission for all users to write to the log file
+	chmod(log_file,  S_IWUSR|S_IWOTH|S_IWGRP | S_IRUSR|S_IROTH|S_IRGRP );	// wr-wr-wr-
+	if (wr_err < 0){ 
+		return -1;
+    }
+	return 0;
+}
+
+// Useful functions from Assignment 2
+int readFromFile(char * filename, unsigned char * data, int * data_len){
+    // FILE *fp;
+   	// fp = fopen(filename, "rb");
+    
+	FILE *original_fopen_ret;
+	FILE *(*original_fopen)(const char*, const char*);
+
+	/* call the original fopen function */
+	original_fopen = dlsym(RTLD_NEXT, "fopen");
+	original_fopen_ret = (*original_fopen)(filename, "rb");
+	
+	if(original_fopen_ret == NULL){
+        return 1;
+    }
+    /* File commands */ 
+    /* (necessary for reading special characters like EOF, etc) */
+    fseek(original_fopen_ret, 0, SEEK_END);     // go to file end
+    *data_len = ftell(original_fopen_ret);           // calculate the file size
+    rewind(original_fopen_ret);                 // go to file start and...
+    if(fread(data, *data_len, sizeof(unsigned char), original_fopen_ret) < 0){
+        fclose(original_fopen_ret);
+        return 1;
+    }
+    fclose(original_fopen_ret);
+    return 0;
+}
+
+// A modified version of print_hex, to convert MD5 hash to a hex string
+void string_to_hex(unsigned char *data, char *out_data, size_t len)
+{
+	size_t i;
+	size_t j;
+
+	if (data) {
+        j=0;
+		for (i = 0;  i < len; i++) {
+			// if (!(i % 16) && (i != 0))
+			// 	printf("\n");
+			sprintf((out_data+j),"%02X", data[i]);
+            j+=2;
+		}
+		// printf("\n");
+	}
+}
